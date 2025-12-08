0e7379d73a51bf452c15974b33a6cc806117809b
Shashavali-0505 <shashavali0505@gmail.com>
2025-12-08 14:50:10 +0530

    pushing-first-project

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from basic.views import home,about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',home,name='home'),
+    path('about/',about,name='about'),
 ]
